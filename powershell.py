97aff9a84581ba24f604189376621e8f2afe5c88
Ddady1 <ddady1@gmail.com>
2024-09-29 15:51:25 +0300

    start

--- powershell.py
+++ powershell.py
@@ -1,5 +1,29 @@
 import subprocess
 
-command = 'Get-Aduser dady -Properties * -ErrorAction Stop | fl Enabled, LockedOut, PasswordExpired, whenCreated'
+command = 'Get-Aduser benc -Properties * -ErrorAction Stop | fl Enabled, LockedOut, PasswordExpired, whenCreated'
 result = subprocess.run(['powershell.exe', command], capture_output=True, text=True)
-print(result.stdout)
+ans = result.stdout
+print(ans)
+print(type(ans))
+print(len(ans))
+strlight = ''
+for i in ans:
+    if i == ' ':
+        continue
+    else:
+        strlight += i
+print(strlight)
+hhhhh = strlight.replace('\n', ' ')
+print(hhhhh)
+starmoon = ''
+for i in strlight:
+    if i == '\n':
+        continue
+    else:
+        starmoon += i
+print(starmoon)
+
+
+
+li = list(ans.split(' '))
+print(li)
