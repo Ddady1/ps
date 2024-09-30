84eccfdfc16816710eafc7fa3bba9172f4e68bc7
Ddady1 <ddady1@gmail.com>
2024-09-30 16:23:23 +0300

    start5

--- powershell.py
+++ powershell.py
@@ -1,4 +1,35 @@
 import subprocess
+import tkinter as tk
+from tkinter import ttk
+from ctypes import windll
+import ttkbootstrap as ttkb
+from ttkbootstrap import Toplevel
+from ttkbootstrap.constants import *
+from PIL import Image, ImageTk
+
+'''img = Image.open('assets/powershell_icon.ico')
+img = img.resize((100, 100), Image.LANCZOS)'''
+
+img = 'assets/powershell_icon.ico'
+
+
+class App(ttkb.Window):
+
+    def __init__(self):
+
+        super(App, self).__init__(themename='sandstone')
+
+        # Creating Master Window
+
+        self.title('Powershell Toolkit')
+        self.geometry('600x400+150+150')
+        self.iconbitmap(img)
+
+
+    windll.shcore.SetProcessDpiAwareness(1)
+
+
+
 
 command = 'Get-Aduser benc -Properties * -ErrorAction Stop | fl Enabled, LockedOut, PasswordExpired, whenCreated'
 result = subprocess.run(['powershell.exe', command], capture_output=True, text=True)
@@ -27,3 +58,7 @@ print(starmoon)
 
 li = list(ans.split(' '))
 print(li)
+
+if __name__ == '__main__':
+    app = App()
+    app.mainloop()
