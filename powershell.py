aa8165e718bbab31ec393407d9bc230b970ee866
Ddady1 <ddady1@gmail.com>
2024-10-08 14:53:46 +0300

    get ad user stat

--- powershell.py
+++ powershell.py
@@ -37,7 +37,7 @@ class App(ttkb.Window):
         azuread_btn.place(x=20, y=120)
         ad_btn = ttkb.Button(text='Active Directory On Prem', command=self.ad_onprem)
         ad_btn.place(x=20, y=160)
-        cancel_btn = ttkb.Button(text='Exit', width=10, command=self.quit)
+        cancel_btn = ttkb.Button(text='Exit', width=10, command=self.destroy)
         cancel_btn.place(x=500, y=350)
 
 
@@ -49,6 +49,10 @@ class App(ttkb.Window):
         root = Toplevel()
         root.title('Active Directory On Prem Tool')
         root.geometry('600x400+170+170')
+        get_ADuser_stat_btn = ttkb.Button(root, text='Get user statistics', command=self.get_aduser_stat_win)
+        get_ADuser_stat_btn.place(x=20, y=20)
+        cancel_btn = ttkb.Button(root, text='Exit', width=10, command=root.destroy)
+        cancel_btn.place(x=500, y=350)
         root.mainloop()
 
 
@@ -56,7 +60,24 @@ class App(ttkb.Window):
         root = Toplevel()
         root.title('Azure Active Directory Tool')
         root.geometry('600x400+170+170')
+        cancel_btn = ttkb.Button(root, text='Exit', width=10, command=root.destroy)
+        cancel_btn.place(x=500, y=350)
         root.mainloop()
+
+    def get_aduser_stat_win(self):
+        root = Toplevel()
+        root.title('Get user statistics')
+        root.geometry('600x300+180+180')
+        user_name = ttkb.StringVar()
+        username_en = ttkb.Entry(root, textvariable=user_name, width=30)
+        username_en.bind("<Button-1>", lambda e: username_en.delete(0, tk.END))
+        username_en.place(x=30, y=50)
+        username_en.insert(0, 'Please enter user name')
+        get_stat_btn = ttkb.Button(root, text='Get Statistics', command=lambda: print(user_name.get()))
+        get_stat_btn.place(x=230, y=50)
+        root.mainloop()
+
+
 
 command = 'Get-Aduser benc -Properties * -ErrorAction Stop | fl Enabled, LockedOut, PasswordExpired, whenCreated'
 result = subprocess.run(['powershell.exe', command], capture_output=True, text=True)
