c384dac29a87c137856f61fe4d84ce93fe2f3161
Ddady1 <ddady1@gmail.com>
2024-11-24 16:38:38 +0200

    checking tool img2

--- rast check.py
+++ rast check.py
@@ -24,13 +24,13 @@ window.geometry('600x400+150+150')
 window.minsize(600, 400)
 #window.iconbitmap(img)
 chk_win_image = Image.open('assets/powershell_img.png')
-re_chk_win_image = chk_win_image.resize((70, 70))
+re_chk_win_image = chk_win_image.resize((50, 50))
 img = ImageTk.PhotoImage(re_chk_win_image)
 chk_win_img_lb = ttkb.Label(window, image=img)
-chk_win_img_lb.place(x=100, y=150)
+chk_win_img_lb.place(x=10, y=10)
 
 main_lbf = ttkb.LabelFrame(window, text="Info", width=580, height=100)
-main_lbf.place(x=10, y=10)
+main_lbf.place(x=10, y=90)
 
 main_lb = ttkb.Label(main_lbf, text='This tool will check if your computer has the neccessary modules in order for the \napp '
                           '"Powershell Toolkit" to work. \nPress the "GO" button to start the checking.', font=('Helvetica', 12), style='primary')
@@ -39,7 +39,7 @@ main_lb.place(x=10, y=10)
 checking_lb = ttkb.Label(text="Checking for necessary dependencies...", font=('Helvetica', 18), foreground='green', state='disabled')
 checking_lb.place_forget()
 
-go_btn = ttkb.Button(main_lbf, text='GO!!!', style='success', command=lambda: checking_lb.place(x=10, y=120))
+go_btn = ttkb.Button(main_lbf, text='GO!!!', style='success', command=lambda: checking_lb.place(x=10, y=200))
 go_btn.place(x=510, y=40)
 
 
