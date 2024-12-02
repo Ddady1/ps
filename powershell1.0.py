f59e68909c4df46c86a3ce90053e5b18dce3ef6e
Ddady1 <ddady1@gmail.com>
2024-12-02 18:17:50 +0200

    bad lock count

--- powershell1.0.py
+++ powershell1.0.py
@@ -92,7 +92,7 @@ def get_user_stats(infolb):
     global info
     command = f'Get-Aduser -identity {username_var.get()} -Properties * -ErrorAction Stop | fl DisplayName, EmailAddress,' \
               f' Enabled, LockedOut, PasswordExpired, PasswordLastSet, whenCreated, BadLogonCount, Title'
-    result = subprocess.run(['powershell.exe', command], capture_output=True, text=True)
+    result = subprocess.run(['powershell.exe', command], capture_output=True, encoding='cp862')
     if result.stdout:
         info = clean_results(result.stdout)
         infolb.config(text=('\n'.join(info)))
